3c32fcf7ac3b18f9400f2fd363397c0114c5465d
ahmetalacam <115187287+ahmetalacam@users.noreply.github.com>
2024-07-19 13:48:12 +0300

    Update file_rename.py

--- file_rename.py
+++ file_rename.py
@@ -1,3 +1,4 @@
+"""
 import os
 
 # Dosyaların bulunduğu dizini belirtin
@@ -19,3 +20,29 @@ for filename in os.listdir(directory):
         # Dosyayı yeniden adlandırın
         os.rename(old_filepath, new_filepath)
         print(f'Renamed: {old_filepath} -> {new_filepath}')
+"""
+
+
+
+import os
+
+# Dosyaların bulunduğu dizini belirtin
+directory = 'path/to/your/directory'
+
+# Eklenecek string
+suffix = '_X'
+
+# Dizindeki tüm dosyaları gezin
+for filename in os.listdir(directory):
+    # Sadece .wav uzantılı dosyaları işlem yapalım
+    if filename.endswith('.wav'):
+        # Yeni dosya adını oluşturun
+        name, ext = os.path.splitext(filename)
+        new_filename = f"{name}{suffix}{ext}"
+        # Eski ve yeni dosya yollarını oluşturun
+        old_filepath = os.path.join(directory, filename)
+        new_filepath = os.path.join(directory, new_filename)
+        # Dosyayı yeniden adlandırın
+        os.rename(old_filepath, new_filepath)
+        print(f'Renamed: {old_filepath} -> {new_filepath}')
+
